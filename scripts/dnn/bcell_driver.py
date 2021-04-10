0f2d3bd9e72bf8413f555fc309fcbe6f49092444
Amritha Musipatla <sm3773@columbia.edu>
2021-04-09 22:46:43 -0400

    Add function to extract data from cytometry data

--- scripts/dnn/bcell_driver.py
+++ scripts/dnn/bcell_driver.py
@@ -5,6 +5,9 @@ import pandas as pd
 # flow cytometry libraries
 import cytoflow as flow
 
+# user defined functions
+import bcell_preprocess as preprocess
+
 marrow_basal_file = 'data/B_cell_data/Marrow1_01_Basal1.fcs'
 marrow_bcr_file   = 'data/B_cell_data/Marrow1_06_BCR.fcs'
 
@@ -20,8 +23,15 @@ import_op = flow.ImportOp(conditions = {'bcr' : 'float'},
 
 ex = import_op.apply()
 
+# Check channels in the experiment
 print('\n')
 print("Channels in this experiment: \n", ex.channels)
 print(len(ex.channels))
 print('\n')
+
+# Extract cytometry data in pandas database format
+cells_db   = preprocess.tube_to_db(ex)
+print(cells_db.head)
+
+
 
--- scripts/dnn/bcell_preprocess.py
+++ scripts/dnn/bcell_preprocess.py
@@ -4,3 +4,9 @@ import pandas as pd
 
 # flow cytometry libraries
 import cytoflow as flow
+
+def tube_to_db(cytometry_experiment):
+    # wrapper function for extracting data for cytoflow experiment
+    cyto_db = cytometry_experiment.data
+    cyto_db = cyto_db.drop(columns=['EventNum', 'Time', 'Cell Length'])
+    return cyto_db
