73aedcbadcbb513d84eeebbf491f9d9a65542549
Amritha Musipatla <sm3773@columbia.edu>
2021-04-10 19:45:11 -0400

    Add command line argument parsing for verbosity and plots

--- scripts/dnn/bcell_driver.py
+++ scripts/dnn/bcell_driver.py
@@ -12,6 +12,17 @@ import cytoflow as flow
 import bcell_preprocess as bpreprocess
 import bcell_plot as bplot
 
+# read command line flags
+parser = argparse.ArgumentParser(description='Run a DNN on cytometry data.')
+parser.add_argument('-v', '--verbose', 
+                    help='Print information and status to console.',
+                    action='store_true')
+parser.add_argument('-p', '--plot', 
+                    help='Display plots.',
+                    action='store_true')
+args = parser.parse_args()
+
+# specify data files
 marrow_basal_file = 'data/B_cell_data/Marrow1_01_Basal1.fcs'
 marrow_bcr_file   = 'data/B_cell_data/Marrow1_06_BCR.fcs'
 
@@ -28,19 +39,25 @@ import_op = flow.ImportOp(conditions = {'bcr' : 'float'},
 ex = import_op.apply()
 
 # Check channels in the experiment
-print('\n')
-print("Channels in this experiment: \n", ex.channels)
-print(len(ex.channels))
-print('\n')
+if args.verbose:
+    # print information about the channels in the experiment
+    print('\n')
+    print('Number of channels: ', len(ex.channels))
+    print("Channels in this experiment: \n", ex.channels)
+    print('\n')
 
 # Extract cytometry data in pandas database format
 cells_df   = bpreprocess.tube_to_df(ex)
-#print(cells_df.head)
-#print(cells_df.dtypes)
+if args.verbose:
+    # print information about the dataframes with cytometry data
+    print(cells_df.head)
+    print(cells_df.dtypes)
 
 # EDA plots
-bplot.value_count_plot(cells_df).plot(kind = 'bar')
-plt.title("Cell Count by Category")
-plt.xlabel("BCR Stimulated/Unstimulated")
-plt.ylabel("Number of Cells")
-plt.show(block=True)
+if args.plot:
+    # Display a plot showing the count per category
+    bplot.value_count_plot(cells_df).plot(kind = 'bar')
+    plt.title("Cell Count by Category")
+    plt.xlabel("BCR Stimulated/Unstimulated")
+    plt.ylabel("Number of Cells")
+    plt.show(block=True)
