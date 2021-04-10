9df7a3ad02e50a60c0ce8bc9e8f6b20f11fd9767
Amritha Musipatla <sm3773@columbia.edu>
2021-04-10 14:40:30 -0400

    Add plot module

--- scripts/dnn/bcell_driver.py
+++ scripts/dnn/bcell_driver.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 
 # visualization
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 # flow cytometry libraries
 import cytoflow as flow
 
 # user defined functions
-import bcell_preprocess as preprocess
+import bcell_preprocess as bpreprocess
+import bcell_plot as bplot
 
 marrow_basal_file = 'data/B_cell_data/Marrow1_01_Basal1.fcs'
 marrow_bcr_file   = 'data/B_cell_data/Marrow1_06_BCR.fcs'
@@ -33,10 +35,14 @@ print(len(ex.channels))
 print('\n')
 
 # Extract cytometry data in pandas database format
-cells_df   = preprocess.tube_to_df(ex)
+cells_df   = bpreprocess.tube_to_df(ex)
 print(cells_df.head)
 
 # EDA plots
-
+bplot.value_count_plot(cells_df).plot(kind = 'bar')
+plt.title("Cell Count by Category")
+plt.xlabel("BCR Stimulated/Unstimulated")
+plt.ylabel("Number of Cells")
+plt.show(block=True)
 
 
